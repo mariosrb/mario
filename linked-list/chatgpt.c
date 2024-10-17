135f93b6cc1e140c5cde96c2519e3d11f7920e36
mario <mario-2000@hotmail.fr>
2024-10-17 12:45:03 +0000

    1

---
+++ linked-list/chatgpt.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct noeux noeux;
+
+struct noeux
+{
+    int data;
+    struct noeux* next;
+};
+#include <stdio.h>
+#include <stdlib.h>
+    strnnnoeux wagon1;
+    wastrstruct chatgpt
+    {
+        /* data */
+    };
+    noeux wagonwagon
+
+int main(int ac, char **av)
+{
+    noeux wagon1;
+    
+
+    return 0;
+}
